667d429dd346ce3b472fd642adc98399c208035a
Julieta <j.zalazar183@gmail.com>
2025-03-03 04:07:26 -0300

    Eliminar archivos innecesarios

--- Escritorio/Ciro- cuidado mascotas/demo/src/main/java/com/example/demo/entidad/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.entidad;
-
-public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN,
-}
--- Escritorio/Ciro- cuidado mascotas/demo/src/main/java/com/example/demo/request/EmailRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.request;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-
-public record EmailRequest(@Email @NotNull String email) {
-
-}
